e9b64bfd864c05256da23535a80cf2987cadcf43
Shanmukh Yenikapati <shanmukh.yenikapati@rutgers.edu>
2023-01-17 00:37:44 -0500

    update script

--- details.sh
+++ details.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Please provide directory address"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Please provide valid directory address"
+    exit 1
+fi
+
+cd "$1"
 
 branch=$(git branch --show-current)
 echo active branch: $branch
@@ -19,7 +30,7 @@ fi
 
 author=$(git log -1 --pretty=format:"%an")
 rufus="Rufus"
-if [[ $author == $rufus ]]; then
+if [ "$author" == "$rufus" ]; then
     echo "blame Rufus: True"
 else
     echo "blame Rufus: False"
